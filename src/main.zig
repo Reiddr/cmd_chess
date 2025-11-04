d7ac700591ab3eb4233cb047454d4bee6db98c91
Dylan <dylan@seedspider.com>
2025-11-05 07:46:17 +1300

    some experiments with colours

--- src/main.zig
+++ src/main.zig
@@ -13,10 +13,14 @@ pub fn main() !void {
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    const green = "\x1b[0;32m";
+    // const green = "\x1b[0;32m";
+    // const black = "\x1b[0;30m";
+    const white = "\x1b[0;37m";
     const reset = "\x1b[0m";
-    const knight = '\u{2658}';
-    try stdout.print("{s}Run `zig build test` to run the {u}tests.\n{s}", .{green, knight, reset});
+    const brown_bg = "\x1b[48;5;94m";
+    const knight_w = '\u{2658}';
+    const knight_b = '\u{265E}';
+    try stdout.print("knight_w: {s}{s}{u}{s} knight_b: {s}{s}{u}{s}\n", .{brown_bg, white, knight_b, reset, brown_bg, white, knight_w, reset});
 
     try bw.flush(); // Don't forget to flush!
 }
