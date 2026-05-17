cbfe64e5f490e02893046e3f636663460b5000af
Reiddr <zechillfactor@gmail.com>
2026-05-17 08:48:51 +0000

    printing a board with white pawns

--- include/print_board.h
+++ include/print_board.h
@@ -9,50 +9,56 @@ char* pb_get_piece_char(BBPieceType pt, int white)
 {
 	switch(pt) {
 		case BB_T_KING:
-			return	(white) ? "\u{2654}" : "\u{265A}" ;
+			return	(white) ? " \u{2654}  " : " \u{265A}  " ;
 		case BB_T_QUEEN:
-			return	(white) ? "\u{2655}" : "\u{265B}" ;
+			return	(white) ? " \u{2655}  " : " \u{265B}  " ;
 		case BB_T_ROOK:
-			return	(white) ? "\u{2656}" : "\u{265C}" ;
+			return	(white) ? " \u{2656}  " : " \u{265C}  " ;
 		case BB_T_BISHOP:
-			return	(white) ? "\u{2657}" : "\u{265D}" ;
+			return	(white) ? " \u{2657}  " : " \u{265D}  " ;
 		case BB_T_KNIGHT:
-			return	(white) ? "\u{2658}" : "\u{265E}" ;
+			return	(white) ? " \u{2658}  " : " \u{265E}  " ;
 		case BB_T_PAWN:
-			return	(white) ? "\u{2659}" : "\u{265F}" ;
+			return	(white) ? " \u{2659}  " : " \u{265F}  " ;
 		default:
 			return	"";
 	}
 }
 
-int pb_print_byte(uint8_t byte, char space, char* colour){
-	int i = 0;
-	printf("%s%c", colour, space);
-	uint8_t bit;
-	for(i=0; i<8; i++){
-		bit = (byte >> i) && 0x01;
-		printf(" %u |", bit);
-	}
-	printf("\n" RESET);
-        return 0;
-}
-
-int pb_print_board(struct BBBoardState bs){
-        size_t len_tmp_buff = 2;
+int pb_print_board(struct BBBoardState bs)
+{
+        size_t len_tmp_buff = 20;
         char tmp_buff[len_tmp_buff];
-        int i;
+        BBPieceType pt = BB_T_PAWN;
+        uint64_t bb = bs.white_pieces[pt];
+        int i, j;
 	for(i = 7; i > -1; i--){
-                //format and print the rank number
-                snprintf(tmp_buff, len_tmp_buff, "%i", i+1);
-                td_puts(tmp_buff, FG_WHITE, BG_BLACK);
-                
-                int j;
-                for(j = 0; j < 8; j++){
-                         
+                int k;
+                for (k = 0; k < 3; k++){
+                        if (k == 1){
+                                //format and print the rank number
+                                snprintf(tmp_buff, len_tmp_buff, "%i  ", i+1);
+                                td_puts(tmp_buff, FG_WHITE, BG_BLACK);
+                        } else {
+                                td_puts( "   ", FG_WHITE, BG_BLACK);
+                        }
+                        for(j = 7; j > -1; j--){
+                                char *fg = FG_WHITE;
+                                char *bg = ((i + j) % 2) ? BG_BROWN : BG_GRAY;
+                                if (k == 1){
+                                        uint64_t mask = 1ULL << (i * 8 + j);
+                                        char *p = (bb & mask) ? pb_get_piece_char(pt, bs.turn_white) : "    ";
+                                        td_puts(p, fg, bg);
+                                } else {
+                                        td_puts("    ", fg, bg);
+                                }
+                        }
+                        td_reset_colour();
+                        putchar('\n');
                 }
-		// rank = bs >> (i*8);
-		// pb_print_byte(rank, '|', FG_YELLOW);
 	}
+        td_puts( "\n    A   B   C   D   E   F   G   H \n", FG_WHITE, BG_BLACK);
+        td_reset_colour();
         return 0;
 }
 #endif /*PRINT_BOARD_H*/
