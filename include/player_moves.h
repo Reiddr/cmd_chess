396b2552bd26046ca1c7aab0ff2b337012f493dd
Reiddr <zechillfactor@gmail.com>
2026-05-26 09:12:09 +0000

    added a get piece function

--- include/player_moves.h
+++ include/player_moves.h
@@ -2,6 +2,7 @@
 #define PLAYER_MOVES_H
 
 #include <stdint.h>
+#include "bitboard.h"
 
 /* moves can be of the form a1b2 [start_square][end_square]
  * this function will check that both are actually on the board
@@ -30,11 +31,32 @@ int pm_check_move_str(const char* s)
  * eg "a1" or "e5"
  * return a bitboard with the square set to 1
  */
-uint64_t pm_get_start_square(const char* s){
+uint64_t pm_get_square(const char* s){
         int file = s[0] - 'a';
         int rank = s[1] - '1';
         uint64_t r = 1ULL << (rank * 8 + (7 - file)); /* 8 - file because we're shifting starting from the right */
         return r;
+}
+
+/* A fucntion to take in the move string and return its starting square, piece type, and colour
+ * does not check validity of string or move
+ * will use the first result found, ie will not check the board state validity
+ * returns 0 if allgood
+ * positive if no piece is found
+ */
+int pm_find_piece(const struct BBBoardState bs, const char* s, uint64_t* start_square, BBPieceType* type, int* white){
+        *start_square = pm_get_square(s);
+        for (*type = BB_T_PAWN; *type < BB_T_COUNT; (*type)++) {
+                if (bs.white_pieces[*type] & *start_square) {
+                        *white = 1;
+                        return 0;
+                }
+                if (bs.black_pieces[*type] & *start_square) {
+                        *white = 0;
+                        return 0;
+                }
+        }
+        return 1;
 }
 
 
--- src/main.c
+++ src/main.c
@@ -12,6 +12,8 @@ int main(){
         pb_get_fen(bs, buff, len_buff);
         printf("FEN str: %s\n\n", buff);
 
+        bb_print_binary(bs.black_pieces[BB_T_KNIGHT]);
+
         pb_print_board(bs);
         printf("\n");
         pb_print_board_fancy(bs);
@@ -30,8 +32,17 @@ int main(){
                         printf("Example: 'e2e4'\n");
                         continue;
                 }
-                printf("starting square: \n");
-                bb_print_binary(pm_get_start_square(input));
+                BBPieceType type;
+                int white;
+                uint64_t start_square;
+                valid = pm_find_piece(bs, input, &start_square, &type, &white);
+                if (valid !=0) {
+                        printf("Did not find a piece on starting square. Your move %s\n", input);
+                }
+                printf("Piece type: %d\n", type);
+                printf("Piece is white: %d\n", white);
+                printf("Starting square: \n");
+                bb_print_binary(pm_get_square(input));
                 /* check valid move */
                 /* play move */
                 /* check for win */
