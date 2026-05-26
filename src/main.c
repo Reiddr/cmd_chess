e3c9aaf837819d6c16c68bc620542e9e85fd46c8
Reiddr <zechillfactor@gmail.com>
2026-05-26 09:16:27 +0000

    tidy up prints in main

--- src/main.c
+++ src/main.c
@@ -12,8 +12,6 @@ int main(){
         pb_get_fen(bs, buff, len_buff);
         printf("FEN str: %s\n\n", buff);
 
-        bb_print_binary(bs.black_pieces[BB_T_KNIGHT]);
-
         pb_print_board(bs);
         printf("\n");
         pb_print_board_fancy(bs);
@@ -23,6 +21,7 @@ int main(){
         while (!win){
                 printf("Enter move: ");
                 scanf("%49s", input);
+
                 /* check valid input */
                 int valid = pm_check_move_str(input);
                 if (valid != 0) {
@@ -32,6 +31,8 @@ int main(){
                         printf("Example: 'e2e4'\n");
                         continue;
                 }
+
+                /* get the piece type */
                 BBPieceType type;
                 int white;
                 uint64_t start_square;
@@ -42,7 +43,7 @@ int main(){
                 printf("Piece type: %d\n", type);
                 printf("Piece is white: %d\n", white);
                 printf("Starting square: \n");
-                bb_print_binary(pm_get_square(input));
+                bb_print_binary(start_square);
                 /* check valid move */
                 /* play move */
                 /* check for win */
