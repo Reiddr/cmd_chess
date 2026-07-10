66f2c19f06b43a4f1be9c7229d0d5c05923454c2
Reiddr <zechillfactor@gmail.com>
2026-07-10 04:40:51 +0000

    WIP updates to board_moves

--- include/board_moves.h
+++ include/board_moves.h
@@ -5,28 +5,53 @@
 #include <stddef.h>
 #include "bitboard.h"
 
-struct PMMove {
+struct BMMove {
         uint64_t start;
         uint64_t end;
+        BBPieceType promotion_piece; /* if piece is a promoting pawn, this is the piece it is promoting to */
 };
 
-struct _move_info; /* internal struct, defined in source, created to pack common move info */
-
 /* gets all the valid moves for a valid board state
- * iterates through all the pieces of the current players turn and creates a list of PMMove
+ * iterates through all the pieces of the current players turn and creates a list of BMMove
  * this will include if pieces are pinned or moving into check, castling etc
  * returns the number of valid moves populated
  * or negative on fail
  */
-int bm_get_valid_moves(const struct BBBoardState bs, struct PMMove *moves, const size_t len_moves);
+int bm_get_valid_moves_board(const struct BBBoardState *bs, struct BMMove *moves, const size_t len_moves);
 
-/* The next set of functions will get the moves for each piece with 
- * respect to the state of the game,
- * returns the number of moves allocated
- * or a negative number if fail
- * these functions will not check for checks, pins, ect.
+/* Gets the valid moves for a single piece
+ * does not handle checks and pins or castling.
+ * does handle en_passant
+ * returns the number of moves set
+ * or negative number on fail
  */
+int bm_get_valid_moves_piece(const struct BBBoardState *bs, const uint64_t start, struct BMMove *moves, const size_t len_moves);
 
-int bm_get_valid_pawn_moves(const struct _move_info mi, const int white, const uint64_t en_passant_square, uint64_t *moves, const size_t len_moves);
+/* Function to apply a move to the board,
+ * will alter the board state
+ * removes castling rights
+ * checks enpassant square
+ * switches the turn
+ * and increments the move clocks
+ */
+int make_move(struct BBBoardState *bs, const struct BMMove *bm);
+
+/* Function to see if the possible moves put the enemy king in check
+ * possible moves is all the moves possible 
+ * returns 0 if not in check
+ * returns 1 if in check
+ */
+int in_check(const struct BBBoardState *bs, const uint64_t possible_moves);
+
+/* Checks if the current board state is valid
+ * that there are no overlapping pieces
+ * that there can only be one king for each side
+ * that there can never be more than 8 pawns for each side
+ * that the turn int is either 0 or 1
+ * that neither side can have more that 16 pieces total
+ * returns 0 if okay
+ * returns positve if not
+ */
+int valid_board_state(const struct BBBoardState *bs);
 
 #endif /*BOARD_MOVES_H*/
--- src/board_moves.c
+++ src/board_moves.c
@@ -3,47 +3,87 @@
 #include "piece_moves.h"
 #include <stdint.h>
 
-struct _move_info {
-        uint64_t start;
-        uint64_t ally_pieces;
-        uint64_t enemy_pieces;
-};
-
-int bm_get_valid_moves(const struct BBBoardState bs, struct PMMove *moves, const size_t len_moves)
+int bm_get_valid_moves_board(const struct BBBoardState *bs, struct BMMove *moves, const size_t len_moves)
 {
-        uint64_t ally_pieces = bb_get_mask((bs.turn_white ? bs.white_pieces : bs.black_pieces), BB_T_COUNT);
-        uint64_t enemy_pieces = bb_get_mask((!bs.turn_white ? bs.white_pieces : bs.black_pieces), BB_T_COUNT);
+        uint64_t ally_pieces, enemy_pieces;
+        int masks_allocated;
+        masks_allocated = bb_merge_masks(&ally_pieces, (bs->turn_white ? bs->white_pieces : bs->black_pieces), BB_T_COUNT);
+        masks_allocated = bb_merge_masks(&enemy_pieces, (!bs->turn_white ? bs->white_pieces : bs->black_pieces), BB_T_COUNT);
         /* iterate through ally pieces and get valid moves for each piece */
         if (!(start & ally_pieces))
                 return -1;
         return 0;
 }
 
-int bm_get_valid_pawn_moves(const struct _move_info mi, const int white, const uint64_t en_passant_square, uint64_t *moves, const size_t len_moves)
+int bm_get_valid_moves_piece(const struct BBBoardState *bs, const uint64_t start, struct BMMove *moves, const size_t len_moves)
 {
-        uint64_t possible_captures, possible_moves;
-        enum {LEN_INDICES = 64};
-        int indices[LEN_INDICES];
-        int valid, num_indices, i, num_moves;
+        uint64_t possible_captures, possible_moves, ally_pieces, enemy_pieces;
+        int valid, num_masks, white;
+        BBPieceType pt;
 
-        valid = pm_get_pawn_moves(mi.start, white, &possible_moves, &possible_captures);
+        valid = bb_merge_masks(&ally_pieces, (bs->turn_white ? bs->white_pieces : bs->black_pieces), BB_T_COUNT);
+        if (valid != 0)
+                return -1;
+        valid = bb_merge_masks(&enemy_pieces, (!bs->turn_white ? bs->white_pieces : bs->black_pieces), BB_T_COUNT);
         if (valid != 0)
                 return -2;
-        num_indices = bb_get_piece_indices(possible_moves, indices, LEN_INDICES);
-        if (num_indices < 0)
+
+        valid = bb_find_piece(bs, start, &pt, &white);
+        if (valid != 0)
                 return -3;
 
-        num_moves = 0;
-        for (i = 0; i < num_indices; i++) {
-                uint64_t bb = BB_1 << indices[i];
-                if (!(bb & mi.ally_pieces))
-                        if ((size_t)(num_moves + 1) > len_moves)
-                                return -1;
-
-                        moves[num_moves++] = bb;
+        switch(pt) {
+        case (BB_T_PAWN):
+                valid = pm_get_pawn_moves(start, bs->turn_white, &possible_moves, &possible_captures);
+                /* need to handle special case of pawns capturing diagonally only if an enemy piece is present or enpassant */
+                possible_captures &= (enemy_pieces | bs->en_passant_square);
+                possible_moves |= possible_captures;
+                break;
+        case (BB_T_KNIGHT):
+                valid = pm_get_knight_moves(start, &possible_moves);
+                break;
+        case (BB_T_BISHOP):
+                valid = pm_get_bishop_moves(start, &possible_moves);
+                break;
+        case (BB_T_ROOK):
+                valid = pm_get_rook_moves(start, &possible_moves);
+                break;
+        case (BB_T_QUEEN):
+                valid = pm_get_queen_moves(start, &possible_moves);
+                break;
+        case (BB_T_KING):
+                valid = pm_get_king_moves(start, &possible_moves);
+                break;
+        default:
+                return -1;
         }
 
-        
+        if (valid != 0)
+                return -2;
 
+        possible_moves &= (possible_moves ^ ally_pieces); /* can't move to spaces occupied by ally pieces */
+
+        num_masks = bb_split_masks(possible_moves, moves, len_moves);
+        if (num_masks < 0)
+                return -3;
+
+        return num_masks;
+}
+
+int make_move(struct BBBoardState *bs, const struct BMMove *bm)
+{
+        return 0;
+}
+
+int in_check(const struct BBBoardState *bs, const uint64_t possible_moves)
+{
+        if (possible_moves & (!bs->turn_white ? bs->white_pieces[BB_T_KING] : bs->black_pieces[BB_T_KING]))
+                return 1;
+        else
+                return 0;
+}
+
+int valid_board_state(const struct BBBoardState *bs)
+{
         return 0;
 }
